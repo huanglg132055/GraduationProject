76353da444036d2ce8d8b2d51ff28a14101d0319
huanglg <huangliguo123>
2021-02-18 15:33:24 +0800

    T-ssm项目搭建

--- src/main/java/com/lenovo/graduation/controller/LoginController.java
+++ src/main/java/com/lenovo/graduation/controller/LoginController.java
@@ -1,24 +1,24 @@
 package com.lenovo.graduation.controller;
 
+import com.alibaba.fastjson.JSON;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 public class LoginController {
 
     @RequestMapping("/")
-    public ModelAndView index(){
+    @ResponseBody
+    public Map<String,Object> index(){
         System.out.println("--------------------------------------------");
-        ModelAndView mv = new ModelAndView("/index");
-        return mv;
+        Map<String,Object> map = new HashMap<>();
+        map.put("iccid","111111");
+        return map;
     }
 
-    @RequestMapping("/index")
-    public ModelAndView queryAll() throws Exception {
-        System.out.println("--------------------------------------------");
-        ModelAndView mv = new ModelAndView("/index");
-        return mv;
-    }
 }
